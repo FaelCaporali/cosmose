bb2a1c0298dba9f225ca8dadfacd62ecc9b189b1
Fael Caporali <rafaelhon@gmail.com>
2023-05-12 23:36:31 -0300

    browser router father of all

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Container, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, BrowserRouter } from 'react-router-dom'
 import Routes from './components/Routes'
 import Provider from './context/Provider'
 import './App.css'
@@ -7,18 +7,20 @@ import './App.css'
 function App() {
 
   return (
-    <Provider>
-      <Container>
-        <Navbar expand="lg" bg='dark' variant='dark' as="nav">
-            <Link to="/" className='text-white'>Menu Inicial</Link>
-            <Link to="/character/new" className='text-white'>Novo Personagem</Link>
-            <Link to="/character" className='text-white'>Personagens</Link>
-        </Navbar>
-      </Container>
-      <Container>
-        <Routes />
-      </Container>
-    </Provider>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/cosmose/'}>
+      <Provider>
+        <Container>
+          <Navbar expand="lg" bg='dark' variant='dark' as="nav">
+              <Link to="/" className='text-white'>Menu Inicial</Link>
+              <Link to="/character/new" className='text-white'>Novo Personagem</Link>
+              <Link to="/character" className='text-white'>Personagens</Link>
+          </Navbar>
+        </Container>
+        <Container>
+          <Routes />
+        </Container>
+      </Provider>
+    </BrowserRouter>
   )
 }
 
